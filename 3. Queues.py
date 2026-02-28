ca4a32d25f02ef767ecd2f61da8aaa9f98b3d8b2
Avishkar Renuse <avishkarrenuse06@gmail.com>
2026-03-01 00:14:18 +0530

    update queues.py

--- 3. Queues.py
+++ 3. Queues.py
@@ -82,3 +82,87 @@ print("Dequeue: ", myQueue.dequeue())
 print("Queue after Dequeue: ", myQueue.queue)
 print("isEmpty: ", myQueue.isEmpty())
 print("Size: ", myQueue.size())
+
+
+'''Queue Implementation using Linked Lists
+A linked list consists of nodes with some sort of data, and a pointer to the next node.
+
+A singly linked list.
+A big benefit with using linked lists is that nodes are stored wherever there is free space in memory, the nodes do not have to be stored contiguously right after each other like elements are stored in arrays. Another nice thing with linked lists is that when adding or removing nodes, the rest of the nodes in the list do not have to be shifted.
+
+To better understand the benefits with using arrays or linked lists to implement queues, you should check out this page that explains how arrays and linked lists are stored in memory.
+
+This is how a queue can be implemented using a linked list.'''
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+class Queue:
+  def __init__(self):
+    self.front = None
+    self.rear = None
+    self.length = 0
+
+  def enqueue(self, element):
+    new_node = Node(element)
+    if self.rear is None:
+      self.front = self.rear = new_node
+      self.length += 1
+      return
+    self.rear.next = new_node
+    self.rear = new_node
+    self.length += 1
+
+  def dequeue(self):
+    if self.isEmpty():
+      return "Queue is empty"
+    temp = self.front
+    self.front = temp.next
+    self.length -= 1
+    if self.front is None:
+      self.rear = None
+    return temp.data
+
+  def peek(self):
+    if self.isEmpty():
+      return "Queue is empty"
+    return self.front.data
+
+  def isEmpty(self):
+    return self.length == 0
+
+  def size(self):
+    return self.length
+
+  def printQueue(self):
+    temp = self.front
+    while temp:
+      print(temp.data, end=" -> ")
+      temp = temp.next
+    print()
+
+# Create a queue
+myQueue = Queue()
+
+myQueue.enqueue('A')
+myQueue.enqueue('B')
+myQueue.enqueue('C')
+
+print("Queue: ", end="")
+myQueue.printQueue()
+print("Peek: ", myQueue.peek())
+print("Dequeue: ", myQueue.dequeue())
+print("Queue after Dequeue: ", end="")
+myQueue.printQueue()
+print("isEmpty: ", myQueue.isEmpty())
+print("Size: ", myQueue.size())
+
+'''Reasons for using linked lists to implement queues:
+
+Dynamic size: The queue can grow and shrink dynamically, unlike with arrays.
+No shifting: The front element of the queue can be removed (enqueue) without having to shift other elements in the memory.
+Reasons for not using linked lists to implement queues:
+
+Extra memory: Each queue element must contain the address to the next element (the next linked list node).
+Readability: The code might be harder to read and write for some because it is longer and more complex.'''
