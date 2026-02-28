c74f84dd24eae7f130e9a46c4cf67b54c78e85ae
Avishkar Renuse <avishkarrenuse06@gmail.com>
2026-03-01 00:25:49 +0530

    summery

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,26 +1,46 @@
+
 class Node:
   def __init__(self, data):
     self.data = data
     self.next = None
 
-def findLowestValue(head):
-  minValue = head.data
-  currentNode = head.next
+def traverseAndPrint(head):
+  currentNode = head
   while currentNode:
-    if currentNode.data < minValue:
-      minValue = currentNode.data
+    print(currentNode.data, end=" -> ")
     currentNode = currentNode.next
-  return minValue
+  print("null")
+
+def insertNodeAtPosition(head, newNode, position):
+  if position == 1:
+    newNode.next = head
+    return newNode
+
+  currentNode = head
+  for _ in range(position - 2):
+    if currentNode is None:
+      break
+    currentNode = currentNode.next
+
+  newNode.next = currentNode.next
+  currentNode.next = newNode
+  return head
 
 node1 = Node(7)
-node2 = Node(11)
-node3 = Node(3)
-node4 = Node(2)
-node5 = Node(9)
+node2 = Node(3)
+node3 = Node(2)
+node4 = Node(9)
 
 node1.next = node2
 node2.next = node3
 node3.next = node4
-node4.next = node5
 
-print("The lowest value in the linked list is:", findLowestValue(node1))
+print("Original list:")
+traverseAndPrint(node1)
+
+# Insert a new node with value 97 at position 2
+newNode = Node(97)
+node1 = insertNodeAtPosition(node1, newNode, 2)
+
+print("\nAfter insertion:")
+traverseAndPrint(node1)
