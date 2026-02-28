8b12d5b23622636d42a313e9fc39923f6f513498
Avishkar Renuse <avishkarrenuse06@gmail.com>
2026-03-01 00:21:44 +0530

    summery

---
+++ 4. Linked Lists.py
@@ -0,0 +1,97 @@
+''' defination of a node in a linked list 
+A Linked List is, as the word implies, a list where the nodes are linked together. Each node contains data and a pointer. The way they are linked together is that each node points to where in the memory the next node is placed.
+The last node in the list points to null, which indicates the end of the list. The first node is called the head of the list. The head is used to access the rest of the nodes in the list.
+The main advantage of linked lists over arrays is that they can grow and shrink in size dynamically,'''
+
+
+'''Types of Linked Lists
+There are three basic forms of linked lists:
+
+Singly linked lists
+Doubly linked lists
+Circular linked lists
+A singly linked list is the simplest kind of linked lists. It takes up less space in memory because each node has only one address to the next node, like in the image below.
+
+A singly linked list.
+A doubly linked list has nodes with addresses to both the previous and the next node, like in the image below, and therefore takes up more memory. But doubly linked lists are good if you want to be able to move both up and down in the list.
+
+A doubly linked list.
+A circular linked list is like a singly or doubly linked list with the first node, the "head", and the last node, the "tail", connected.
+
+In singly or doubly linked lists, we can find the start and end of a list by just checking if the links are null. But for circular linked lists, more complex code is needed to explicitly check for start and end nodes in certain applications.
+
+Circular linked lists are good for lists you need to cycle through continuously.
+
+The image below is an example of a singly circular linked list:
+
+A circular singly linked list.
+The image below is an example of a doubly circular linked list:
+
+A circular doubly linked list.
+
+Linked List Operations
+Basic things we can do with linked lists are:
+
+Traversal
+Remove a node
+Insert a node
+Sort
+For simplicity, singly linked lists will be used to explain these operations below.'''
+
+#Traversal of linked list
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+def traverseAndPrint(head):
+  currentNode = head
+  while currentNode:
+    print(currentNode.data, end=" -> ")
+    currentNode = currentNode.next
+  print("null")
+
+node1 = Node(7)
+node2 = Node(11)
+node3 = Node(3)
+node4 = Node(2)
+node5 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+traverseAndPrint(node1)
+
+
+#Finding the lowest value in a singly linked list in Python:
+
+# class Node:
+#   def __init__(self, data):
+#     self.data = data
+#     self.next = None
+
+def findLowestValue(head):
+  minValue = head.data
+  currentNode = head.next
+  while currentNode:
+    if currentNode.data < minValue:
+      minValue = currentNode.data
+    currentNode = currentNode.next
+  return minValue
+
+node1 = Node(7)
+node2 = Node(11)
+node3 = Node(3)
+node4 = Node(2)
+node5 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+print("The lowest value in the linked list is:", findLowestValue(node1))
+
+
