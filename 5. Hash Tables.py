e75dba565bccee76f1382583b33ec0a17b24a253
Avishkar Renuse <avishkarrenuse06@gmail.com>
2026-03-02 21:38:30 +0530

    code added

--- 5. Hash Tables.py
+++ 5. Hash Tables.py
@@ -19,3 +19,14 @@ Create a hash function.
 Inserting an element using a hash function.
 Looking up an element using a hash function.
 Handling collisions.'''
+
+#Create a Hash Function that sums the Unicode numbers of each character and return a number between 0 and 9:
+
+def hash_function(value):
+  sum_of_chars = 0
+  for char in value:
+    sum_of_chars += ord(char)
+
+  return sum_of_chars % 10
+
+print("'Bob' has hash code:", hash_function('Bob'))
