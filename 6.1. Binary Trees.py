ed7486c2ef1b43544f186f97b44b4e2119e39b6b
Avishkar Renuse <avishkarrenuse06@gmail.com>
2026-03-04 10:48:20 +0530

    update python trees

---
+++ 6.1. Binary Trees.py
@@ -0,0 +1,43 @@
+'''A tree is a hierarchical data structure consisting of nodes connected by edges.
+
+Each node contains a value and references to its child nodes.'''
+
+"""Binary Trees
+A Binary Tree is a type of tree data structure where each node can have a maximum of two child nodes, a left child node and a right child node.
+
+This restriction, that a node can have a maximum of two child nodes, gives us many benefits:
+
+Algorithms like traversing, searching, insertion and deletion become easier to understand, to implement, and run faster.
+Keeping data sorted in a Binary Search Tree (BST) makes searching very efficient.
+Balancing trees is easier to do with a limited number of child nodes, using an AVL Binary Tree for example.
+Binary Trees can be represented as arrays, making the tree more memory efficient.
+Binary Trees are the foundation for more complex tree types like Binary Search Trees and AVL Trees, which we will cover in the next pages."""
+
+class TreeNode:
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+
+root = TreeNode('R')
+nodeA = TreeNode('A')
+nodeB = TreeNode('B')
+nodeC = TreeNode('C')
+nodeD = TreeNode('D')
+nodeE = TreeNode('E')
+nodeF = TreeNode('F')
+nodeG = TreeNode('G')
+
+root.left = nodeA
+root.right = nodeB
+
+nodeA.left = nodeC
+nodeA.right = nodeD
+
+nodeB.left = nodeE
+nodeB.right = nodeF
+
+nodeF.left = nodeG
+
+# Test
+print("root.right.left.data:", root.right.left.data)
