77146e8d04fd36d18a5ec09e332c1c2e999ca53d
Avishkar Renuse <avishkarrenuse06@gmail.com>
2026-03-05 10:09:49 +0530

    more code added

--- 7. Binary Search Tree.py
+++ 7. Binary Search Tree.py
@@ -88,4 +88,19 @@ if result:
   print(f"Found the node with value: {result.data}")
 else:
   print("Value not found in the BST.")
-  
+
+
+#Inserting a node in a BST:
+
+def insert(node, data):
+  if node is None:
+    return TreeNode(data)
+  else:
+    if data < node.data:
+      node.left = insert(node.left, data)
+    elif data > node.data:
+      node.right = insert(node.right, data)
+  return node
+
+# Inserting new value into the BST
+insert(root, 10)
