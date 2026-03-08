90120e9db0768041f8e31341d28b9a4492953e1e
Avishkar Renuse <avishkarrenuse06@gmail.com>
2026-03-08 23:15:33 +0530

    Adding new file with new code.

--- 9. Graphs.py
+++ 9. Graphs.py
@@ -11,3 +11,25 @@ Maps and Navigation: Locations, like a town or bus stops, are stored as vertices
 Internet: Can be represented as a Graph, with web pages as vertices and hyperlinks as edges.
 Biology: Graphs can model systems like neural networks or the spread of diseases.
 '''
+
+"""Graph Representations
+A Graph representation tells us how a Graph is stored in memory.
+
+Different Graph representations can:
+
+take up more or less space.
+be faster or slower to search or manipulate.
+be better suited depending on what type of Graph we have (weighted, directed, etc.), and what we want to do with the Graph.
+be easier to understand and implement than others.
+Below are short introductions of the different Graph representations, but Adjacency Matrix is the representation we will use for Graphs moving forward in this tutorial, as it is easy to understand and implement, and works in all cases relevant for this tutorial.
+
+Graph representations store information about which vertices are adjacent, and how the edges between the vertices are. Graph representations are slightly different if the edges are directed or weighted.
+
+Two vertices are adjacent, or neighbors, if there is an edge between them.
+
+Adjacency Matrix Graph Representation
+Adjacency Matrix is the Graph representation (structure) we will use for this tutorial.
+
+How to implement an Adjacency Matrix is shown on the next page.
+
+The Adjacency Matrix is a 2D array (matrix) where each cell on index (i,j) stores information about the edge from vertex i to vertex j."""
