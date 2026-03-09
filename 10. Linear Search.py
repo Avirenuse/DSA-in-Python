ced0c1903eea9bece1e0abc64ad04fad2992a832
Avishkar Renuse <avishkarrenuse06@gmail.com>
2026-03-09 22:06:15 +0530

    Adding new file.

---
+++ 10. Linear Search.py
@@ -0,0 +1,12 @@
+'''Linear Search
+Linear search (or sequential search) is the simplest search algorithm. It checks each element one by one.
+
+This algorithm is very simple and easy to understand and implement.
+
+How it works:
+
+Go through the array value by value from the start.
+Compare each value to check if it is equal to the value we are looking for.
+If the value is found, return the index of that value.
+If the end of the array is reached and the value is not found, return -1 to indicate that the value was not found.
+If the array is already sorted, it is better to use the much faster Binary Search algorithm that we will explore on the next page.'''
